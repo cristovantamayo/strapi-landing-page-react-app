24aca2a491619b85b86b856de72a947b11ad501c
Cristovan Tamayo <ctamayo@bionexo.com>
2025-01-13 21:19:26 -0300

    Jest & jest-styled-components

---
+++ src/templates/Home/Home.test.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { screen } from "@testing-library/react";
+import { describe, expect, it } from "@jest/globals";
+import { renderTheme } from "../../styles/render-theme";
+import Home from ".";
+
+it("renders learn react link", () => {
+  const { debug } = renderTheme(<Home />);
+  const headingContainer = screen.getByRole("heading", {
+    name: "Hello World",
+  }).parentElement;
+
+  expect(headingContainer).toHaveStyle({
+    background: "black",
+    color: "white",
+  });
+
+  expect(headingContainer).toHaveStyleRule("background", "black");
+
+  expect(headingContainer).toMatchSnapshot();
+
+  debug();
+});
