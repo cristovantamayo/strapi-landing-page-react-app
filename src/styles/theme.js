b26ff582001acb27ce088c8807f4c0524ea215ab
Cristovan Tamayo <ctamayo@bionexo.com>
2025-01-20 20:44:43 -0300

    MenuLink Component and Tests

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -23,4 +23,14 @@ export const theme = {
   media: {
     lessMedium: "(max-width: 768px)",
   },
+  spacings: {
+    xSmall: "0.8rem",
+    small: "1.6rem",
+    medium: "2.4rem",
+    large: "3.2rem",
+    xLarge: "4.0rem",
+    xXLarge: "4.8rem",
+    huge: "5.6rem",
+    xHuge: "6.4rem",
+  },
 };
