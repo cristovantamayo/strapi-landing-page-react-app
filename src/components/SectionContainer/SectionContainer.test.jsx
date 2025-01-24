d5508eb8c70ff03114cab8fe154fa4d19ef48b78
Cristovan Tamayo <ctamayo@bionexo.com>
2025-01-24 08:05:48 -0300

    SectionBackground-and-SectionContainer-tests

--- src/components/SectionContainer/SectionContainer.test.jsx
+++ src/components/SectionContainer/SectionContainer.test.jsx
@@ -4,8 +4,13 @@ import { renderTheme } from "../../styles/render-theme";
 import { SectionContainer } from ".";
 
 describe("<SectionContainer />", () => {
-  it("should render the component", () => {
-    renderTheme(<SectionContainer>Children</SectionContainer>);
+  it("should render the content container", () => {
+    const { container } = renderTheme(
+      <SectionContainer>
+        <h1>Children</h1>
+      </SectionContainer>,
+    );
     expect(screen.getByRole("heading")).toBeInTheDocument();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
