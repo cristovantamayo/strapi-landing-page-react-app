6c686643f1ecb86bb2c9272ab605de83bceaab8a
Cristovan Tamayo <ctamayo@bionexo.com>
2025-01-26 11:35:49 -0300

    Headings Fix

--- src/components/GridTwoColumns/index.jsx
+++ src/components/GridTwoColumns/index.jsx
@@ -9,7 +9,7 @@ export const GridTwoColumns = ({ title, text, srcImg, background = false }) => {
     <SectionBackground background={background}>
       <Styled.Container background={background}>
         <Styled.TextContainer>
-          <Heading upperCase colorDark={!background}>
+          <Heading upperCase colorDark={!background} as="h2">
             {title}
           </Heading>
           <TextComponent>{text}</TextComponent>
