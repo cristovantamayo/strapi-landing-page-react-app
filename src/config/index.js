3f03d81a0d426eb5a17773cf9f4192694f185382
Cristovan Tamayo <ctamayo@bionexo.com>
2025-02-22 11:49:34 -0300

    Set Heroku App Adress

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
+// url: "http://localhost:1337/api/pages?filters[slug][$eq]=",
 export default {
-  url: "http://localhost:1337/api/pages?filters[slug][$eq]=",
+  url: "https://strapi-landing-page-84e3fa2a7657.herokuapp.com/api/pages?filters[slug][$eq]=",
   siteName: "Cristovan Tamayo",
   defaultSlug: "landing-page",
 };
